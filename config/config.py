e37bc61cceb9aef4c4249615db010b6bc8f86283
orochidrake <andrezaguette@gmail.com>
2023-07-31 16:31:48 -0300

    add python-doenv for better organization

---
+++ config/config.py
@@ -0,0 +1,19 @@
+from pydantic import BaseSettings
+from dotenv import load_dotenv
+from functools import lru_cache
+
+load_dotenv(".env")
+
+class Settings(BaseSettings):
+    app_name: str = "Credit Card Crud API"
+    PORT: str
+    
+    class Config:
+        env_file = ".env"
+
+
+@lru_cache()
+def get_settings():
+    return Settings()
+
+settings = get_settings()
